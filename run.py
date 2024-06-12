6a806ced0d16ffaffc1fa55c843a8f34fdf83ef8
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-06-12 21:10:38 +0700

    update run

--- run.py
+++ run.py
@@ -30,4 +30,4 @@ api.add_resource(resources.GetUser, '/api/user')
 api.add_resource(resources.Predict, '/api/predict')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT", 8080))
