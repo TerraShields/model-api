1907d0a901601e16b32fcbf5d8dab1554d7fda61
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-06-13 21:36:03 +0700

    update requirements

--- load_model.py
+++ load_model.py
@@ -9,10 +9,6 @@ custom_objects = {"KerasLayer": hub.KerasLayer}
 model_classification = tf.keras.models.load_model(
     app.config['MODEL_PATH'], compile=False, custom_objects=custom_objects)
 
-model_wind_spread = tf.keras.models.load_model(
-    app.config['WIND_SPREAD_PATH'], compile=False, custom_objects=custom_objects)
-
-
 classess = [
     "ants",
     "bees",
@@ -45,8 +41,3 @@ def image_classification(req_image):
 
     return result
 
-
-def wind_spread_classification(pest_name):
-    result = model_wind_spread.predict(pest_name)
-
-    return result
